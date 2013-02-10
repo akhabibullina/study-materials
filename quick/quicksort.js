1c65b200da910a0d039752e4c521d480c666cb88
akhabibullina <ascii88@mail.ru>
2013-02-10 21:32:49 +0300

    Some improvements of he algorithm, testing data is added.

--- quick/quicksort.js
+++ quick/quicksort.js
@@ -1,23 +1,28 @@
+var totalNumberOfComparisons = 0;
+
 function quicksort(array) {
+  console.log('input array: '+array);
+    if (array.length <= 1) {
+        return array;
+    }
 
-   if (array.length <= 1) {
-      return array;
-   }
-
-   // Choose pivot
-   var pivotRandIndex = getRandomNumber(0, array.length - 1);
-   // Swap pivot to the 1st element in the array -- to ease the algoritm.
-   array.swap(0, pivotRandIndex);
+  // var pivotIndex = choosePivot(array.length-1, 'first');
+  // array.swap(0, pivotIndex);
 
    var pivotIndex = 0;
    var pivot = partition(array, pivotIndex);
+   console.log('pivot index: '+pivot);
+   console.log('pivot value: '+array[pivotIndex]);
 
    // Create before-pivot and after-pivot arrays
-   var left  = array.slice(0, pivot);
-   var right = array.slice(pivot, array.length);
-   left.swap(0, left.length-1);
-
-   return quicksort(left).concat(quicksort(right));
+   var left  = array.slice(1, pivot);
+   var right = array.slice(pivot);
+   
+   console.log('left: '+left);console.log('right: '+right);
+   
+   totalNumberOfComparisons += array.length - 1;
+   console.log('# of comparisons: '+totalNumberOfComparisons);
+   return quicksort(left).concat(array[pivotIndex], quicksort(right));
 }
 
 function partition(array, pivotIndex) {
@@ -35,10 +40,33 @@ function getRandomNumber(min, max) {
    return Math.floor((Math.random()*max)+min); 
 }
 
+function getTotalNumberOfComparisons() {
+   return totalNumberOfComparisons;
+}
+function setTotalNumberOfComparisons() {
+	totalNumberOfComparisons = 0;
+}
+
+function choosePivot(length, position) {
+   var pivotIndex;
+   switch (position) {
+      case 'first':
+         pivotIndex = 0;
+         break;
+      case 'final':
+         pivotIndex = length;
+         break;
+      case 'randrom':
+         pivotIndex = getRandomNumber(0, length);
+        break;
+      default:
+         alert ('choose pivot failed');
+   }
+   return pivotIndex;
+}
+
 Array.prototype.swap = function(a, b) {
    var tmp=this[a];
    this[a]=this[b];
    this[b]=tmp;
 }
-
-document.write(quicksort([5, 2, 3, 1, 4, 6, 0, 2, 5, 6, 7, 9, 4, 3, 1, 7, 10, 12, 15, 17, 18, 11, 14, 13, 16]));
