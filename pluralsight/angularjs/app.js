3be339c54d0afab6c6cee6389ea74aebc99aedde
akhabibullina <akhabibullina@aisquared.com>
2015-08-06 11:04:43 -0400

    Angular Route init - add missing files.

---
+++ pluralsight/angularjs/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by admin on 7/28/2015.
+ */
+(function() {
+  var app = angular.module('MyGithubApp', ["$scope", "github", "$interval", '$log', '$anchorScroll', '$location', 'ngRoute']);
+
+  //app.config(function($routeProvider){
+  //  $routeProvider
+  //    .when('/main', {
+  //      template: 'index.html',
+  //      controller: 'MainController'
+  //    })
+  //    .otherwise({redirectTo: 'main'})
+  //})
+
+});
