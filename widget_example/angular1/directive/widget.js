d0ada988e84e143bcfbd644ec51b0c53351a836e
akhabibullina <akhabibullina@aisquared.com>
2016-01-06 10:31:03 -0500

    Sync the feature in examples of angular1 and angular2.

--- widget_example/angular1/directive/widget.js
+++ widget_example/angular1/directive/widget.js
@@ -6,12 +6,13 @@ myApp.directive('widget', [function () {
         restrict: 'E', //E = element, A = attribute, C = class, M = comment
         template:
         '<section class="container">' +
-           '<div id="box1" class="box" ng-click="toggle()" ng-class="state"> Box </div>' +
+           '<div id="box1" class="box" ng-click="toggle()" ng-class="stateClass"> Box </div>' +
         '</section>',
         link: function ($scope, element, attrs) {
             $scope.state = 'default';
             $scope.toggle = function (inp) {
                 $scope.state = $scope.state === 'default'? 'full': 'default';
+                $scope.stateClass = $scope.state;
             };
         }
     }
--- widget_example/angular2/component/widget.js
+++ widget_example/angular2/component/widget.js
@@ -1,13 +1,22 @@
 /**
  * Created by annakh on 1/6/16.
  */
-(function(app) {
-  app.AppComponent = ng.core
-    .Component({
-      selector: 'my-app',
-      template: '<h1>My First Angular 2 App</h1>'
-    })
-    .Class({
-      constructor: function() {}
-    });
+(function (app) {
+    app.AppComponent = ng.core
+        .Component({
+            selector: 'widget',
+            template: '<section class="container">' +
+            '<div id="box1" (click)="toggle($event)" class="box {{stateClass}}"> Box </div>' +
+            '</section>',
+        })
+        .Class({
+            constructor: function () {
+                this.state = 'default';
+            },
+            toggle: function (e) {
+                // todo: don't attach to the mouse event
+                this.state = this.state === 'default' ? 'full' : 'default';
+                this.stateClass = this.state;
+            }
+        });
 })(window.app || (window.app = {}));
